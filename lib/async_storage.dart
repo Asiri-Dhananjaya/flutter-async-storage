8e1041c1e217bd3341d7eb556c57c545e5583f02
Asiri Dhananjaya Madhuranga <asiridananjaya733@gmail.com>
2023-12-22 14:01:07 +0530

    async_storage_flutter

--- lib/async_storage.dart
+++ lib/async_storage.dart
@@ -1,15 +1,24 @@
-library async_storage;
-
+import 'dart:convert'; // Import the 'dart:convert' library
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AsyncStorage {
   static const FlutterSecureStorage _storage = FlutterSecureStorage();
 
-  static Future<void> setItem(String key, String value) async {
-    await _storage.write(key: key, value: value);
+  static Future<void> setItem(String key, dynamic value) async {
+    // Convert the value to a JSON string
+    String jsonValue = jsonEncode(value);
+    await _storage.write(key: key, value: jsonValue);
   }
 
-  static Future<String?> getItem(String key) async {
-    return await _storage.read(key: key);
+  static Future<T?> getItem<T>(String key) async {
+    // Retrieve the JSON string from storage
+    String? jsonValue = await _storage.read(key: key);
+
+    // If the value is null, return null
+    if (jsonValue == null) return null;
+
+    // Decode the JSON string to the specified data type
+    T? value = jsonDecode(jsonValue);
+    return value;
   }
 }
